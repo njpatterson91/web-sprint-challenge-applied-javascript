d4a8c0ee79902c7d5f75c5078634c8d7e444aeae
Nathaniel Patterson <odst0016@gmail.com>
2020-09-04 12:04:57 -0400

    only need event listener

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,52 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+import axios from "axios";
+
+const cardsEntryPoint = document.querySelector(".cards-container");
+
+function cards(props) {
+  //instantiating
+  const divCard = document.createElement("div");
+  const divHeadline = document.createElement("div");
+  divHeadline.textContent = props.headline;
+  const divAuthor = document.createElement("div");
+  const divImg = document.createElement("div");
+  const imgSrc = document.createElement("img");
+  imgSrc.src = props.authorPhoto;
+  const authSpan = document.createElement("span");
+  authSpan.textContent = props.authorName;
+  //applying content
+
+  //setting styles
+  divCard.classList.add("card");
+  divHeadline.classList.add("headline");
+  divAuthor.classList.add("author");
+  divImg.classList.add("img-container");
+  //consturcting object
+  divCard.appendChild(divHeadline);
+  divCard.appendChild(divAuthor);
+  divAuthor.appendChild(divImg);
+  divImg.appendChild(imgSrc);
+  divAuthor.appendChild(authSpan);
+
+  return divCard;
+}
+
+axios
+  .get("https://lambda-times-api.herokuapp.com/articles")
+  .then((props) => {
+    //console.log(props.data.articles);
+    const data = props.data.articles;
+    for (const i in data) {
+      console.log("test", data[i]);
+      for (const j in data[i]) {
+        console.log(data[i][j]);
+        // cardsEntryPoint(cards(data[i][j])
+        cardsEntryPoint.appendChild(cards(data[i][j]));
+      }
+    }
+  })
+  .catch((err) => {
+    console.log("This aint it chief", err);
+  });
